8f636329362a1f9b3ed0001b909b910cea94dae8
Zainap16 <zvanblerck1@gmail.com>
2024-08-20 04:35:50 +0200

    completed basicj js computer program via BroCode Tut

---
+++ main.js
@@ -0,0 +1,22 @@
+const decreasebtn = document.getElementById('decrease');
+const resetbtn = document.getElementById('reset');
+const increasebtn = document.getElementById('increase');
+const countLabel = document.getElementById('countLbl');
+
+let count = 0;
+
+increasebtn.onclick =  function(){
+    count++;
+    countLabel.textContent = `${count}`;
+}
+
+
+resetbtn.onclick = function(){
+    count = 0;
+    countLabel.textContent = `${count}`;
+}
+
+decreasebtn.onclick =  function(){
+    count--;
+    countLabel.textContent = `${count}`;
+}
