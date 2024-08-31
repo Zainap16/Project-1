9b025d5ed6264030eb908c89fe710865b9338b7c
Zainap16 <zvanblerck1@gmail.com>
2024-08-31 08:18:20 +0200

    update

--- main.js
+++ main.js
@@ -27,3 +27,54 @@ roll.addEventListener('click', function() {
     num = Math.floor(Math.random() * 101);
     rollLbl.textContent = `${num}`;
 })
+
+// GUESS THE NUMBER
+/*
+get user input
+btn submit
+lbl to display if correct or not
+
+harder: allow 3 attepts only
+*/
+
+let guessInput = document.getElementById("guess");
+
+let btnSubmit = document.getElementById("btnSubmit");
+
+let displaylbl = document.getElementById("displaylbl");
+
+let answer = 5;
+
+btnSubmit.addEventListener('click', function(){
+    let userGuess = parseInt(guessInput.value);
+    if ( userGuess === answer) {
+
+        displaylbl.textContent =  `Correct: ${guess}` ;
+        displaylbl.style.color = 'green';
+        
+    } else {
+        displaylbl.textContent = `Incorrect: ${guess}`;
+        displaylbl.style.color = 'red';
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
